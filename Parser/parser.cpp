0d024cbd58413a748686933502f92c66c52dd371
Bordak Mate <mate@webtar.hu>
2021-11-10 03:04:49 +0100

    CAFF parser almost done

--- Parser/parser.cpp
+++ Parser/parser.cpp
@@ -10,16 +10,62 @@ using namespace std;
 
 std::vector<uint8_t> caffFileData;
 long long num_anim = 0;
+long long year = 0;
+long long month = 0;
+long long day = 0;
+long long hour = 0;
+long long minute = 0;
+string creator = "";
 bool header_parsed = false;
 bool credits_parsed = false;
+int parsed_frames = 0;
+
+
+
+// Source: https://stackoverflow.com/questions/180947/base64-decode-snippet-in-c
+static std::string base64_encode(const std::string& in) {
+
+	std::string out;
+
+	int val = 0, valb = -6;
+	for (char c : in) {
+		val = (val << 8) + c;
+		valb += 8;
+		while (valb >= 0) {
+			out.push_back("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[(val >> valb) & 0x3F]);
+			valb -= 6;
+		}
+	}
+	if (valb > -6) out.push_back("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[((val << 8) >> (valb + 8)) & 0x3F]);
+	while (out.size() % 4) out.push_back('=');
+	return out;
+}
+
+
+string getJsonData() {
+	string result = "";
+	result += "{numAnim:" + num_anim + "}";
+	return result;
+}
+
 
 long long readBytesAsInt(long startIndex, int length)
 {
 	long long result = 0;
 	long long pos = 0;
 	for (long i = startIndex; i < startIndex + length; i++) {
-		result += ((long long)caffFileData[i]) << (pos * 8);		
+		result += ((long long)caffFileData[i]) << (pos * 8);
 		pos++;
+	}
+	return result;
+}
+
+string readBytesAsString(long startIndex, int length)
+{
+	string result = "";
+	for (long i = startIndex; i < startIndex + length; i++) {
+		char c = (char)caffFileData[i];
+		result += c;
 	}
 	return result;
 }
@@ -33,21 +79,30 @@ struct Blockheader {
 void read_caff_header_data(long startIndex)
 {
 	if (!(caffFileData[startIndex] == 'C' && caffFileData[startIndex + 1] == 'A' && caffFileData[startIndex + 2] == 'F' && caffFileData[startIndex + 3] == 'F')) {
-		//TODO hiba: nincs magic CAFF
-		cout << "BAJ!";
+		cerr << "CAFF magic string missing!";
 	}
 	long long caff_header_size = readBytesAsInt(startIndex + 4, 8); // ez amúgy tök fölös?
-	cout << "headerBlockSize:" << (int)caff_header_size << std::endl;
 	num_anim = readBytesAsInt(startIndex + 12, 8);
 	cout << "num_anim:" << (int)num_anim << std::endl;
 }
 
 void read_caff_credits_data(long startIndex)
 {
-	long long year = readBytesAsInt(startIndex, 3);
+	year = readBytesAsInt(startIndex, 2);
+	month = readBytesAsInt(startIndex + 2, 1);
+	day = readBytesAsInt(startIndex + 3, 1);
+	hour = readBytesAsInt(startIndex + 4, 1);
+	minute = readBytesAsInt(startIndex + 5, 1);
 
+	long long lengthOfCreatorField = readBytesAsInt(startIndex + 6, 8);
+	creator = readBytesAsString(startIndex + 14, lengthOfCreatorField);
 }
 
+void read_caff_animation_data(long startIndex)
+{
+	long long duration_milisecs = readBytesAsInt(startIndex, 8);
+	cerr << "TODO";
+}
 
 Blockheader read_block_header(int startIndex) {
 	int blocktype = (int)caffFileData[startIndex];
@@ -63,24 +118,36 @@ int read_block(long startIndex)
 	Blockheader blockheader = read_block_header(startIndex);
 
 	if (blockheader.blocktype == 1) {
-		if (header_parsed) {
-			cout << "BAJ1!";
+		if (header_parsed)
+		{
+			cerr << "There's more than 1 header block";
 		}
 		header_parsed = true;
 		read_caff_header_data(startIndex + 9);
 	}
 	else if (blockheader.blocktype == 2) {
-		if (header_parsed || credits_parsed) {
-			cout << "BAJ2!";
+		if (!header_parsed)
+		{
+			cerr << "The first block must be header block!";
 		}
+		if (credits_parsed)
+		{
+			cerr << "There's more than 1 credits block!";
+		}
+
 		credits_parsed = true;
 		read_caff_credits_data(startIndex + 9);
 	}
 	else if (blockheader.blocktype == 3) {
-
+		if (!header_parsed)
+		{
+			cerr << "The first block must be header block!";
+		}
+		read_caff_animation_data(startIndex + 9);
+		parsed_frames++;
 	}
 	else {
-		// TODO hiba: nem 1,2,3 a block jelzo
+		cerr << "The block type must be 1, 2 or 3!";
 	}
 	return startIndex + 9 + blockheader.blocklength;
 }
@@ -105,15 +172,11 @@ int main()
 
 	std::ifstream stream("1_mod.caff", std::ios::in | std::ios::binary);
 	caffFileData = std::vector<uint8_t>(std::istreambuf_iterator<char>(stream), std::istreambuf_iterator<char>());
-
-	// for(auto i: caffFileData) {
-	// 	int value = i;
-	// 	std::cout << "data: " << value << std::endl;
-	// 	return 0;
-	// }
-
-	int res = read_block(0);
-	read_block(res);
+	long long fileByteCount = caffFileData.size();
+	long long position = 0;
+	do {
+		position = read_block(position);
+	} while (position < fileByteCount);
 
 
 	uint8_t elsoByte = caffFileData[0];
